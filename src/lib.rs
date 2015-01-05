46746b1128a3674e66d42ebdb63dd06babf33ba2
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-01-05 23:04:50 +0530

    Return 0 `score` when `needle` is not a subsequence of `haystack`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
-pub fn score(_haystack: &[u8], needle: &[u8]) -> u16 {
+pub fn score(haystack: &[u8], needle: &[u8]) -> u16 {
     if needle.len() == 0 {
         return 0
     }
 
+    if !is_subsequence_of(needle, haystack) {
+        return 0
+    }
+
     unimplemented!()
+}
+
+fn is_subsequence_of(smaller: &[u8], larger: &[u8]) -> bool {
+    let mut larger = larger.iter();
+
+    for s in smaller.iter() {
+        loop {
+            match larger.next() {
+                Some(l) if s == l => break,
+                Some(_)           => {},
+                None              => return false
+            }
+        }
+    }
+
+    true
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -12,5 +12,11 @@ speculate! {
                 assert_eq!(0, ::fuzzy::score(b"", b""));
             }
         }
+
+        context "needle not a subsequence of haystack" {
+            it "returns 0" {
+                assert_eq!(0, ::fuzzy::score(b"abc", b"xyz"));
+            }
+        }
     }
 }
